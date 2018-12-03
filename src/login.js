2d9f4d970097f510d61a6e22c68bc78d7a34468d
Akiva Schiff <akivaschiff@gmail.com>
2018-12-03 12:32:35 +0200

    print the login error

--- src/login.js
+++ src/login.js
@@ -38,7 +38,8 @@ function initClient() {
     authorizeButton.onclick = handleAuthClick;
     signoutButton.onclick = handleSignoutClick;
   }, function(error) {
-    setErrorPage(JSON.stringify(error, null, 2));
+    console.error(JSON.stringify(error, null, 2));
+    setErrorPage(JSON.stringify(error, null, 2)); 
   });
 }
 
